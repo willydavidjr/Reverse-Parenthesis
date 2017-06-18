676d52cf6a5f08c3c1de49355299a66075fb7e65
willydavidjr <willydavidjr@yahoo.com>
2017-06-19 00:48:04 +0800

    Initial.

--- Reverse Parenthesis/Program.cs
+++ Reverse Parenthesis/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Reverse_Parenthesis
@@ -10,6 +11,123 @@ namespace Reverse_Parenthesis
     {
         static void Main(string[] args)
         {
+            string strPolynomial = @"23 * x ^ 8 + 53 * x - -97 * x ^ 5 - -44 * x ^ 2 + 36 * x - 85 * x ^ 3 - -23 * x";
+            int result = derivative(strPolynomial, -4);
+
+            string strReverseParenthesis = @"The ((quick (brown) (fox) jumps over the lazy) dog)";
+            string resultReverse = reverseParentheses(strReverseParenthesis);
+
+        }
+
+        static int derivative(string polynomial, int x)
+        {
+
+            string[] intPolynomial = polynomial.Split(new string[] { " + ", " - " }, StringSplitOptions.None);
+
+            return 1;
+        }
+
+        static string reverseParentheses(string s)
+        {
+            List<string> lstResult = new List<string>();
+            char[] strResultRegex = Regex.Replace(s, "[A-Za-z]", "").Where(x => x != ' ').ToArray();
+            bool blCheckInside = false;
+
+            if (string.Join("", strResultRegex) == "()()")
+                blCheckInside = true;
+            if (strResultRegex.Where(x => x == '(' || x == ')').Count() == 0)
+                return s;
+
+            string[] strTemp = s.Split(new char[] { '(', ')' });
+            string[] strEven = strTemp.Select((a, b) => new { Value = a, Index = b })
+            .Where(x => x.Index % 2 != 0).Select(y => new string(y.Value.Reverse().ToArray())).Reverse().ToArray();
+
+            string[] strEvenForElseStatement = strTemp.Select((a, b) => new { Value = a, Index = b })
+            .Where(x => x.Index % 2 != 0).Select(y => new string(y.Value.Reverse().ToArray())).ToArray();
+
+            string[] strOdd = strTemp.Select((a, b) => new { Value = a, Index = b })
+            .Where(x => x.Index % 2 == 0).Select(y => y.Value).ToArray();
+
+            int intCounter = 0;
+            int intCounterOdd = 0;
+            var intCounterParenthesis1 = s.Where(x => x == '(').Count();
+            int intCounterParenthesis = s.Where(x => x == '(').Count();
+            int intCountParent = 0;
+            if (strResultRegex[0] == strResultRegex[1])
+            {
+                for (int item = 0; item < strTemp.Length; item++)
+                {
+                    if (item % 2 == 0)
+                    {
+                        lstResult.Add(strOdd[intCounterOdd]);
+
+                        intCounterOdd++;
+                        if (intCountParent <= intCounterParenthesis)
+                        {
+                            intCountParent++;
+                        }
+                        else
+                        {
+                            intCountParent++;
+                        }
+                    }
+                    else
+                    {
+                        lstResult.Add(strEven[intCounter]);
+
+                        intCounter++;
+                        if (intCountParent <= intCounterParenthesis)
+                        {
+                            intCountParent++;
+                        }
+                        else
+                        {
+                            intCountParent++;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int item = 0; item < strTemp.Length; item++)
+                {
+                    if (item % 2 == 0)
+                    {
+                        lstResult.Add(strOdd[intCounterOdd]);
+
+                        intCounterOdd++;
+                        if (intCountParent <= intCounterParenthesis)
+                        {
+                            intCountParent++;
+                        }
+                        else
+                        {
+                            intCountParent++;
+                        }
+                    }
+                    else
+                    {
+                        lstResult.Add(strEvenForElseStatement[intCounter]);
+
+                        intCounter++;
+                        if (intCountParent <= intCounterParenthesis)
+                        {
+                            intCountParent++;
+                        }
+                        else
+                        {
+                            intCountParent++;
+                        }
+                    }
+                }
+            }
+            //string[] strResult = strTemp
+            //.Select(y => new string(y.Reverse().ToArray()))
+            //.Reverse()
+            //.ToArray();
+
+            string result = string.Join("", lstResult.ToArray());
+            return result;
         }
     }
 }
